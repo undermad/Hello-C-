2aaa9bd740150748a90e380f1e11f38942723540
Dominik Tworek <dtworek94@gmail.com>
2024-04-07 02:02:46 +0100

    Abstracted tax calculator and added reading from user functionality. Create fahrenheit to celsius converter function.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 
+
 using namespace std;
+
+void simpleTaxCalculations();
+
+double readNumberFromConsole(string message);
+
+double fahrenheitToCelsius(double fahrenheits);
 
 int main() {
 
-    double sales = 95000;
-    int stateTaxPercentage = 4;
-    int countyTaxPercentage = 2;
+    cout << fahrenheitToCelsius(111);
+    simpleTaxCalculations();
+    return 0;
+}
+
+double fahrenheitToCelsius(double fahrenheits) {
+    return ((fahrenheits - 32) * 5) / 9;
+}
+
+double readNumberFromConsole(string message) {
+    cout << message << endl;
+    double value;
+    cin >> value;
+    return value;
+}
+
+void simpleTaxCalculations() {
+    double sales = readNumberFromConsole("Enter the tax amount:");
+    const double stateTaxPercentage = readNumberFromConsole("Enter the state tax percentage:");
+    const double countyTaxPercentage = readNumberFromConsole("Enter the county tax percentage:");
 
     double stateTax = (sales / 100) * stateTaxPercentage;
     double countyTax = (sales / 100) * countyTaxPercentage;
 
     cout << "Total sales: $" << sales << endl
-            << "State tax: $" << stateTax << endl
-            << "County tax: $" << countyTax << endl
-            << "Total tax: $" << stateTax + countyTax;
-
-
-    return 0;
+         << "State tax: $" << stateTax << endl
+         << "County tax: $" << countyTax << endl
+         << "Total tax: $" << stateTax + countyTax;
 }
+
